fb24a510bd4dfe6775dc09fedbd41606fcc5faa3
Diego Viana <diegovianagomes01@gmail.com>
2023-05-20 17:45:45 -0300

    Formulário

---
+++ script.js
@@ -0,0 +1,32 @@
+emailjs.init("sua Public Key");
+
+document.getElementByID("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const serviceID = "seu serviceID";
+    const templateID = "seu TemplateIS";
+
+    let buttonSubmit = document.querySelector("form-submit");
+
+    emailjs.sendForm(serviceID, templateID, this).then(
+        () => {
+            buttonSubmit.innerHTML = "Enviado com Sucesso!";
+            buttonSubmit.style.backgroundColor = "#00FA9A";
+
+            setTimeout(() => {
+                document.querySelector("#user_name").value = "";
+                document.querySelector("#user_lastname").value = "";
+                document.querySelector("#user_email").value = "";
+                document.querySelector("#message").value = "";
+
+                buttonSubmit.innerHTML = "Enviar";
+                buttonSubmit.style.backgroundColor = "#016266"
+            }, 2000);
+        },
+        (err) => {
+            buttonSubmit.innerHTML = "Por favor tente novamente!";
+            buttonSubmit.style.backgroundColor = "#FF0000"
+            console.log(JSON.stringify(err));
+        }
+    );
+});
